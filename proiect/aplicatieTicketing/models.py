15dcb2525c724bbea21125147a233a3cf6a32c3b
araduflorin <araduflorin@yahoo.com>
2023-10-27 15:14:16 +0300

    Ver27

--- proiect/aplicatieTicketing/models.py
+++ proiect/aplicatieTicketing/models.py
@@ -15,18 +15,6 @@ class Contact(models.Model):
         return f"{self.name}{self.email}{self.message}"
 
 
-class Ticket(models.Model):
-    subject_ticket = models.CharField(max_length=30)
-    status = models.CharField(max_length=15)
-    type = models.CharField(max_length=15)
-    name = models.CharField(max_length=50)
-    email = models.EmailField(max_length=40)
-    user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
-
-    def __str__(self):
-        return f"{self.subject_ticket}{self.status}{self.type}{self.name}{self.email}"
-
-
 class Status(models.Model):
     name = models.CharField(max_length=50)
     description = models.TextField(max_length=500)
@@ -41,6 +29,23 @@ class Type(models.Model):
 
     def __str__(self):
         return f"{self.name}{self.description}"
+
+
+class Ticket(models.Model):
+    subject_ticket = models.CharField(max_length=30)
+    # status = models.CharField(max_length=15)
+    status = models.ForeignKey(Status, on_delete=models.CASCADE)
+    type = models.ForeignKey(Type, on_delete=models.CASCADE)
+    # type = models.CharField(max_length=15)
+    name = models.CharField(max_length=50)
+    email = models.EmailField(max_length=40)
+    user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.subject_ticket}{Status.name}{self.type}{self.name}{self.email}"
+
+
+
 
 
 
